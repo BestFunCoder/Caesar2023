c891343c6fc05d5bec0d59a061fb9f1a36378885
BestFunCoder <oleggutsol80@gmail.com>
2023-07-22 22:56:14 +0300

    fixed class

--- src/Greeting.java
+++ src/Greeting.java
@@ -1,7 +1,6 @@
 public  class Greeting {
     public static final String TEXT_GREETING = """
         === Вітаю в програмі-криптоаналізаторі \"Caesar\"! ===
-        
         """;
     public void greeting(){
         System.out.println(TEXT_GREETING);
