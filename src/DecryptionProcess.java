917a5a4ff3dc59df9418af0ff26f16916bab0119
BestFunCoder <oleggutsol80@gmail.com>
2023-07-28 15:28:31 +0300

    -	renaming variables; -	 comments removed;

--- src/DecryptionProcess.java
+++ src/DecryptionProcess.java
@@ -5,54 +5,41 @@ public class DecryptionProcess {
     SampleUkrainianAlphabet sampleUkrainianAlphabet = new SampleUkrainianAlphabet();
     ArrayList<Character> alphabetUAWithPunctuation = sampleUkrainianAlphabet.getAlphabetUAWithPunctuation();
     ArrayList<Character> punctuation = sampleUkrainianAlphabet.getPunctuation();
-    ArrayList<Character> arrayListOutputFile = new ArrayList<>();
+    ArrayList<Character> OutputFile = new ArrayList<>();
 
-    static List<Character> arrayListSourceEncryptFile = new ArrayList<>();
+    static List<Character> SourceEncryptFile = new ArrayList<>();
     int key;
 
-    public void setArrayListEncryptFile(List<Character> arrayListSourceEncryptFile) {
-        this.arrayListSourceEncryptFile = arrayListSourceEncryptFile;
+    public void setArrayListEncryptFile(List<Character> SourceEncryptFile) {
+        DecryptionProcess.SourceEncryptFile = SourceEncryptFile;
     }
 
-    public ArrayList<Character> decryptionRun (int key){
+    public ArrayList<Character> decryptionRun(int key) {
         this.key = key;
-        for (Character ch : arrayListSourceEncryptFile) {
-            //отримуємо чар з масиву-джерела
-
+        for (Character ch : SourceEncryptFile) {
             if (punctuation.contains(ch)) {
-                //encryptionCore(ch);
-                arrayListOutputFile.add(decryptionCore(ch));
-            }
-
-            //перевірка чи чар є UpperCase
-            else if (Character.isUpperCase(ch)) {
-                arrayListOutputFile.add(decryptionCore(ch));
-
-
-                // якщо чар LowerCase
+                OutputFile.add(decryptionCore(ch));
+            } else if (Character.isUpperCase(ch)) {
+                OutputFile.add(decryptionCore(ch));
             } else if (Character.isLowerCase(ch)) {
-                // Переводимо чар в юперКейс і перевіряємо чи є в українській мові
                 char chUpperCase = Character.toUpperCase(ch);
                 char tempCh = decryptionCore(chUpperCase);
-                arrayListOutputFile.add(Character.toLowerCase(tempCh));
-
+                OutputFile.add(Character.toLowerCase(tempCh));
             }
         }
-        return arrayListOutputFile;
+        return OutputFile;
     }
 
     public char decryptionCore(char ch) {
-        char tempChar = '$';
-        int tempKey = 0;
+        char tempChar = ' ';
+        int tempKey;
         if (alphabetUAWithPunctuation.contains(ch)) {
             int i = alphabetUAWithPunctuation.indexOf(ch);
             tempKey = i - key;
-            //зациклення для алфавіту
             if (tempKey < 0) {
                 tempKey = alphabetUAWithPunctuation.size() - Math.abs(tempKey);
             }
             tempChar = alphabetUAWithPunctuation.get(tempKey);
-
         }
         return tempChar;
     }
