75c6ab0e295b889227babb8914e070ff6ac18016
BestFunCoder <oleggutsol80@gmail.com>
2023-07-28 13:51:19 +0300

    renaming methods and renaming class extends

--- src/EndMenu.java
+++ src/EndMenu.java
@@ -1,4 +1,4 @@
-public final class EndMenu extends ChoiceOfAction{
+public final class EndMenu extends AbstractActionSelectionMenu {
 
     public final String MENU_ITEMS = """
             1 - Вихід в головне меню.
@@ -6,29 +6,28 @@ public final class EndMenu extends ChoiceOfAction{
             """;
 
     @Override
-    public String getMenuItems(){
+    public String getMenuItems() {
         return MENU_ITEMS;
     }
 
     @Override
-    public void getSelectionPosition1(){
+    public void executingMenuItem1() {
         new Menu().starting();
     }
 
     @Override
-    public void getSelectionPosition2(){
+    public void executingMenuItem2() {
         System.exit(0);
     }
 
-
     @Override
-    public void getSelectionPosition3(){
+    public void executingMenuItem3() {
         System.out.println(super.ERROR_CHOICE);
         operationSelection();
     }
 
     @Override
-    public void getSelectionPosition4() {
+    public void executingMenuItem4() {
         System.out.println(super.ERROR_CHOICE);
         operationSelection();
     }
